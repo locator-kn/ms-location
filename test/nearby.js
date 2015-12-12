1303e27d808ef6bce76bf882510eaa44140d9d7f
Timo Weiß <info@timo-weiss.com>
2015-12-12 02:08:43 +0100

    add tests for getLocationsNearby

---
+++ test/nearby.js
@@ -0,0 +1,25 @@
+'use strict';
+import test from 'ava';
+const proxyquire =  require('proxyquire');
+
+const databaseStub = require('./stubs/database.stub');
+const nearby = proxyquire('../lib/nearby', { './database': databaseStub });
+
+test('getLocationsNearby - totally wrong input data', t => {
+    nearby.getLocationsNearby({crap: 'crappapa', bla: 'random'}, (err, data) => {
+        if(err) {
+            return t.pass();
+        }
+        t.faill();
+    });
+});
+
+
+test('getLocationsNearby - right keys wrong datatypes', t => {
+    nearby.getLocationsNearby({lat: 'crappapa', long: 'random', options: {}}, (err, data) => {
+        if(err) {
+            return t.fail();
+        }
+        t.pass();
+    });
+});
