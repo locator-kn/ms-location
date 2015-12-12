71259a61283831278a5185e7cb34d60db5fdabda
Timo Weiß <info@timo-weiss.com>
2015-12-12 01:36:36 +0100

    setup db connection before init seneca

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const seneca = require('seneca')();
 const module = require('./lib/module');
+const datebase = require('./lib/database');
 
 require('dotenv').config({path: '../.env'});
 
@@ -8,9 +9,13 @@ require('dotenv').config({path: '../.env'});
 const transportMethod = process.env['SENECA_TRANSPORT_METHOD'] || 'rabbitmq';
 const patternPin = 'role:location';
 
-// init seneca and expose functions
-seneca
-    .use(transportMethod + '-transport')
-    .add(patternPin + ',cmd:login', module.doSomething)
-    .add(patternPin + ',cmd:else', module.doSomethingElse)
-    .listen({type: transportMethod, pin: patternPin});
+// init database
+datebase.connect().then(() => {
+
+    // init seneca and expose functions
+    seneca
+        .use(transportMethod + '-transport')
+        .add(patternPin + ',cmd:login', module.doSomething)
+        .add(patternPin + ',cmd:else', module.doSomethingElse)
+        .listen({type: transportMethod, pin: patternPin});
+});
