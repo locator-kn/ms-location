5d4b6c0406eb4e39a7b6e966876d41f634f90541
Timo Weiß <info@timo-weiss.com>
2016-01-08 18:19:25 +0100

    typo

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ datebase.connect().then(() => {
         .add(patternPin + ',cmd:locationById', location.getLocationById)
         .add(patternPin + ',cmd:toggleFavor', location.toggleFavorLocation)
         .add(patternPin + ',cmd:addimpression,type:text', location.addTextImpression)
+        .add(patternPin + ',cmd:getlocationstream', location.getLocationStreamById)
         //.act({
         //    role: 'location',
         //    cmd: 'addimpression',
