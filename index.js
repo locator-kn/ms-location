0ba23113c0dcf6afed23dfb4bfcec6b27a66c9cc
Silvia Goetti <sigoetti@htwg-konstanz.de>
2016-01-13 20:21:59 +0100

    fixes and some tweaks in newLoc

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const datebase = require('./lib/database');
 const nearby = require('./lib/nearby');
 const schoenhier = require('./lib/schoenhier');
 const location = require('./lib/location');
+const newLoc = require('./lib/newLoc');
 
 
 // select desired transport method
@@ -26,9 +27,9 @@ datebase.connect().then(() => {
         .add(patternPin + ',cmd:addschoenhier', schoenhier.addSchoenhier)
         .add(patternPin + ',cmd:nearbyschoenhier', schoenhier.getSchoenhiersNearby)
         .add(patternPin + ',cmd:locationById', location.getLocationById)
-        .add(patternPin+',cmd:addnewlocation',newLoc.addNewLocation)
+        .add(patternPin + ',cmd:addnewlocation',newLoc.addNewLocation)
         .add(patternPin+',cmd:locationbyname',location.getLocationByTitle)
-        .listen({type: transportMethod, pin: patternPin});
+
         .add(patternPin + ',cmd:toggleFavor', location.toggleFavorLocation)
         .add(patternPin + ',cmd:addimpression,type:text', location.addTextImpression)
         .add(patternPin + ',cmd:getlocationstream', location.getLocationStreamById)
@@ -46,5 +47,6 @@ datebase.connect().then(() => {
         //    console.log(err || data);
         //})
         //.listen({type: transportMethod, pin: patternPin});
-        .listen({type: 'tcp', port: 7001, pin: patternPin});
+
+        .listen({type: 'tcp', port: 7001,host: '127.0.0.1', pin: patternPin});
 });
