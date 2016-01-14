48cf23910a0644a3f5d1ffc32cca14dbc7e9e3a8
Silvia Goetti <sigoetti@htwg-konstanz.de>
2016-01-14 23:58:12 +0100

    remove location by id

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ datebase.connect().then(() => {
         .add(patternPin + ',cmd:nearbyschoenhier', schoenhier.getSchoenhiersNearby)
         .add(patternPin + ',cmd:locationById', location.getLocationById)
         .add(patternPin + ',cmd:addnewlocation',newLoc.addNewLocation)
+        .add(patternPin + ',cmd:deletelocation',newLoc.deleteLocation)
         //.add(patternPin+',cmd:locationbyname',location.getLocationByTitle)
         .add(patternPin+',cmd:getlocbyuserid',location.getLocationsOfUser)
         .add(patternPin + ',cmd:toggleFavor', location.toggleFavorLocation)
